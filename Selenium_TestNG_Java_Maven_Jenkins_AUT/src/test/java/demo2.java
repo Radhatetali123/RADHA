8b97e6c0eec19c19cb76e8db0ffa9092984f3d6d
Radhatetali123 <136052566+Radhatetali123@users.noreply.github.com>
2023-06-09 11:06:07 +0530

    Add files via upload

---
+++ Selenium_TestNG_Java_Maven_Jenkins_AUT/src/test/java/demo2.java
@@ -0,0 +1,61 @@
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+public class demo2 {
+    WebDriver driver = null;
+
+    @DataProvider(name = "data-set")
+    public static Object[][] DataSet() {
+        Object[][] obj = {
+                {"valid","student", " Password123" },
+                {"Invalid","Radha", "1234" }
+        };
+        return obj;
+    }
+    @BeforeMethod
+    public void setUp() {
+        //System.out.println("\n\n \t Iam inside setUp method");
+        String url = "https://practicetestautomation.com/practice-test-login/";
+        String chromeDriverPath = "C:\\software\\chromedriver_win32 (3)\\chromedriver.exe";
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+        driver = new ChromeDriver();
+        driver.get(url);
+        driver.manage().timeouts().implicitlyWait(10000, TimeUnit.MILLISECONDS);
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(100);
+        driver.quit();
+    }
+    @Test(dataProvider = "data-set")
+    public void search(String type,String username, String password) {
+        System.out.println("type=" + type +" "+"username= " + username + " " + "password=" + password);
+        driver.findElement(By.id("username")).sendKeys(username);
+        driver.findElement(By.id("password")).sendKeys(password);
+        driver.findElement(By.id("submit")).click();
+
+        if (type.equals("valid")) {
+            System.out.println("login successfully");
+        }
+        else
+        {
+            System.out.println("Invalid login");
+        }
+
+    }
+}
+
